14cbaea1b1d1b417b90d2c4d04b23ac34105045a
Dngn20 <dngn20@gmail.com>
2017-02-22 13:04:12 -0700

    Made Project and made panel

---
+++ src/regex/controller/RegexRunner.java
@@ -0,0 +1,10 @@
+package regex.controller;
+
+public class RegexRunner 
+{
+	public static void main (String [] args)
+	{
+		RegexController RegexappController = new RegexController();
+		RegexappController.start();
+	}
+}
